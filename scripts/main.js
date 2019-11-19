55255e99e37cc5d122a9a25e2359b9406f5e8166
Шейдушка <shade0602@yandex.ru>
2019-11-19 17:36:17 +0400

    'first02'

--- scripts/main.js
+++ scripts/main.js
@@ -15,14 +15,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
 	var myName = prompt('Введите свое имя.');
 	localStorage.setItem('name', myName);
-	myHeading.textContent = 'Brachypelima vagans милые, ' + myName;
+	myHeading.textContent = 'Tliltocatl vagans милые, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
 	setUserName();
 } else {
 	var storedName = localStorage.getItem('name');
-	myHeading.textContent = 'Brachypelima vagans милые, ' + storedName;
+	myHeading.textContent = 'Tliltocatl vagans милые, ' + storedName;
 }
 
 myButton.onclick = function() {
